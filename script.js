db9a35f9fafabd0aad4793de831500e8c03de4a7
Andre07G <eleal087@gmail.com>
2025-05-29 22:55:11 -0500

    feat: :sparkles: Function to register a pet
    Function to register a pet was created
    
    by@Andres

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let mascotas = [];
 let dueños = [];
 let id = 0;
+let idMascota= 0;
 let salir = false;
 var menu = "Menu Principal\n1.Registrar Clientes\n2.Registrar Mascotas\n3.Lista de las Mascotas\n4.Buscar Mascotas\n5.Actualizar Estado de Salud\n6.Eliminar Mascota\n7.Ver Mascotas por Cliente\n8.Salir"
 
@@ -14,7 +15,7 @@ setTimeout(() => {
                 registrarDueño();
                 break;
             case "2":
-                verCliente();
+                registrarMascota();
                 break;
             case "3":
                 break;
@@ -51,9 +52,35 @@ function registrarDueño() {
         while (isNaN(telefono)) { telefono = Number(prompt("Numero invalida, intente nuevamente")) }
         let correoElectronico = prompt("Ingrese su correo electronico");
         while (!correoElectronico.includes("@")) { correoElectronico = prompt("Correo invalido, intenta nuevamente") };
-        let dueño = {cedula: cedula, nombre: nombre, telefono: telefono, correo: correoElectronico };
+        let dueño = {id:id,cedula: cedula, nombre: nombre, telefono: telefono, correo: correoElectronico };
         dueños.push(dueño);
+        id++;
         alert("Dueño guardado correctamente");
     }
 }
 
+/*funciones mascotas*/
+
+function registrarMascota() {
+    let cedula = Number(prompt("Ingrese la cedula del dueño"));
+    let cedulaExiste = dueños.find(dueño => dueño.cedula === cedula);
+    let index = dueños.indexOf(cedulaExiste);
+    if (!cedulaExiste) { alert("El cliente no existe");return; } else {
+        let nombre = prompt("Ingrese el nombre");
+        while (nombre.length === 0) { nombre = prompt("No puede dejar este espacio vacio, ingrese nuevamente") };
+        let especie = prompt("Ingrese la especie");
+        while (especie.length === 0) { especie = prompt("No puede dejar este espacio vacio, ingrese nuevamente") };
+        let edad = Number(prompt("Ingrese la edad"));
+        while (isNaN(edad) || edad<=0) { edad = Number(prompt("Numero invalido, intente nuevamente")) }
+        let peso = Number(prompt("Ingrese el peso"));
+        while (isNaN(peso) || peso<=0) { peso = Number(prompt("Numero invalido, intente nuevamente")) }
+        let estado = prompt("Ingresa el estado de salud(sano/enfermo/tratamiento)");
+        estado = estado.toLowerCase();
+        while (estado!="sano" && estado!="enfermo" && estado!="tratamiento"){alert("Estado invalido");estado = prompt("Ingresa el estado de salud(sano/enfermo/tratamiento)");estado=estado.toLowerCase();}
+        let idDueño=dueños[index].id;
+        let mascota = {idMascota:idMascota,nombre: nombre, especie: especie,edad:edad,peso:peso,estado:estado,idDueño:idDueño,dueño:cedula};
+        mascotas.push(mascota);
+        idMascota++;
+        alert("Se registró la mascota exitosamente")
+    }
+}
